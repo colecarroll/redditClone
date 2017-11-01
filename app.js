84499753ae8c693b3350de6cae478bcede39e4a7
Cole Carroll <colecarroll2@gmail.com>
2017-10-31 19:15:16 -0600

    completed user story 4, we can now see existing links and votes, links hyperlink also

--- app.js
+++ app.js
@@ -3,16 +3,24 @@ var app = express();
 require('dotenv').config()
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const queries = require('./db/queries');
 
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
 
 
 
 app.get('/', (req, res)=>{
-res.render('index')
+  queries.getLinks()
+  .then(links => {
+    console.log(links)
+res.render('index', { links: links})
+  })
 })
 
 app.listen(port, () => {
--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,10 @@
 const db = require('./connections')
+
+function getLinks() {
+  return db.select('*').from('links').returning(['linkName', 'url', 'votes'])
+}
+
+
+module.exports = {
+  getLinks
+}
